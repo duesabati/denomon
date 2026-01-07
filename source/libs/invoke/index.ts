a9874974618b4677aa9d00a4e90865c8fd8e7332
Ali Sabtan <ali.sbtan96@gmail.com>
2026-01-07 22:20:08 +0100

    chore: remove unused lib

--- source/libs/invoke/index.ts
+++
@@ -1,4 +0,0 @@
-export * from  './src/action.ts'
-export * from  './src/bound.ts'
-export * from  './src/emitter.ts'
-export * from  './src/toolset.ts'
--- source/libs/invoke/src/bound.ts
+++
@@ -1,8 +0,0 @@
-export function bound<C extends object, T extends (...args: any[]) => any>(
-  target: T,
-  context: ClassMethodDecoratorContext<C, T>,
-) {
-  context.addInitializer(function () {
-    Object.defineProperty(target, 'name', { value: target.name })
-  })
-}
