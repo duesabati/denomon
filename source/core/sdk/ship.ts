2bb571290b97562e32fa5c315d09fe056c73e59e
Ali Sabtan <ali.sbtan96@gmail.com>
2026-03-28 08:48:11 +0000

    feat(core/sdk): add ship command arguments parsing

---
+++ source/core/sdk/ship.ts
@@ -0,0 +1,39 @@
+import * as Kits from '@denomon/core-kits'
+import { parseArgs } from '@std/cli'
+
+export type Parsed = Kits.Ship.Options & Kits.Ship.Configuration & {
+  src: string
+}
+
+export const Parse = (args: string[]): Parsed => {
+  const parsed = parseArgs(args, {
+    boolean: ['watch'],
+    string: ['env-dir', 'out-dir'],
+    default: {
+      'env-dir': 'production',
+    },
+    stopEarly: true,
+  })
+
+  return {
+    environment: parsed['env-dir'],
+    out: parsed['out-dir'],
+    watch: parsed['watch'],
+    src: String(parsed._[0]),
+  }
+}
+
+export const Env = (): Record<string, string | undefined> => {
+  const current = Deno.env.toObject()
+
+  const build: Record<string, string | undefined> = {}
+
+  for (const [key, value] of Object.entries(current)) {
+    if (key.startsWith(Kits.Ship.ENV_PREFIX)) {
+      const envKey = key.substring(Kits.Ship.ENV_PREFIX.length)
+      build[envKey] = value
+    }
+  }
+
+  return build
+}
